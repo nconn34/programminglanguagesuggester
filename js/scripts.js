d7d1468c7525a4f38b77bcdc78fda30734099660
Nathan Conn <nconn34@gmail.com>
2021-11-10 16:03:06 -0800

    took out console.log and cleaned up white space

--- js/scripts.js
+++ js/scripts.js
@@ -4,40 +4,24 @@ $(document).ready(function() {
   $('#lang3').hide();
 
   $("#question1").submit(function(event) {
-
     event.preventDefault()
-  
     const quest1 = parseInt($("#quest1").val());
     const quest2 = parseInt($("#quest2").val());
     const quest3 = parseInt($("#quest3").val());
     const quest4 = parseInt($("#quest4").val()); 
     const quest5 = parseInt($("#quest5").val());
 
-  
-
-
     let price = quest1+quest2+quest3+quest4+quest5
 
-
-
    if (price >= 3 && price <= 8) {
-      console.log(">=3 && <= 8");
       $('#lang1').show();
     } else if (price >= 8 && price<=13) {
-      console.log(">=8 && <= 13");
       $('#lang2').show();
     } else if (price >= 13) {
-      console.log(">=13");
       $('#lang3').show();
     }
-
-
-    console.log($('#question1'));
-;
-
-
   });
-
+  
   $("#goaway").click(function() {
     $('#lang1').hide("");
     $('#lang2').hide("");
